560fa6eda7fcd880bbe1dbc4b5b9c3f53cabe670
LIUCBK <liucbk@bc.edu>
2025-02-16 20:12:44 -0500

    Initial Commit

---
+++ DungeonDice/DungeonDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDiceApp.swift
+//  DungeonDice
+//
+//  Created by 刘佳雨 on 2025/2/16.
+//
+
+import SwiftUI
+
+@main
+struct DungeonDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
